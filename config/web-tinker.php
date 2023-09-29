c7e2526d9de3c770dce28aa0a01c81b6ed8e71f5
kbdxbt <1194174530@qq.com>
2023-09-29 20:06:21 +0800

    update

---
+++ config/web-tinker.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+
+    /*
+     * The web tinker page will be available on this path.
+     */
+    'path' => '/tinker',
+
+    /*
+     * Possible values are 'auto', 'light' and 'dark'.
+     */
+    'theme' => 'auto',
+
+    /*
+     * By default this package will only run in local development.
+     * Do not change this, unless you know what you are doing.
+     */
+    'enabled' => env('APP_ENV') === 'local',
+
+    /*
+    * This class can modify the output returned by Tinker. You can replace this with
+    * any class that implements \Spatie\WebTinker\OutputModifiers\OutputModifier.
+    */
+    'output_modifier' => \Spatie\WebTinker\OutputModifiers\PrefixDateTime::class,
+
+    /*
+    * These middleware will be assigned to every WebTinker route, giving you the chance
+    * to add your own middlewares to this list or change any of the existing middleware.
+    */
+    'middleware' => [
+        Illuminate\Cookie\Middleware\EncryptCookies::class,
+        Illuminate\Session\Middleware\StartSession::class,
+        Spatie\WebTinker\Http\Middleware\Authorize::class,
+    ],
+
+    /*
+     * If you want to fine-tune PsySH configuration specify
+     * configuration file name, relative to the root of your
+     * application directory.
+     */
+    'config_file' => env('PSYSH_CONFIG', null),
+];
